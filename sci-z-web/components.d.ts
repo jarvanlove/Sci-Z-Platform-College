d4b6b27046abe966dca0b20bf73e7d918cc97428
jarvanlove <jarvanlove@users.noreply.github.com>
2025-12-12 15:02:31 +0800

    fix: components.d.ts line ending fix

--- sci-z-web/components.d.ts
+++ sci-z-web/components.d.ts
@@ -32,6 +32,8 @@ declare module 'vue' {
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
+    ElCollapse: typeof import('element-plus/es')['ElCollapse']
+    ElCollapseItem: typeof import('element-plus/es')['ElCollapseItem']
     ElCollapseTransition: typeof import('element-plus/es')['ElCollapseTransition']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDialog: typeof import('element-plus/es')['ElDialog']
