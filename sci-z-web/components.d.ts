9037b3cdbcb5b6f7581b1de88ae170e94226ffa6
jarvanlove <jarvanlove@users.noreply.github.com>
2026-01-15 16:48:37 +0800

    doc：git文件

--- sci-z-web/components.d.ts
+++ sci-z-web/components.d.ts
@@ -33,7 +33,6 @@ declare module 'vue' {
     ElAlert: typeof import('element-plus/es')['ElAlert']
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElButton: typeof import('element-plus/es')['ElButton']
-    ElCard: typeof import('element-plus/es')['ElCard']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCheckboxGroup: typeof import('element-plus/es')['ElCheckboxGroup']
     ElCollapse: typeof import('element-plus/es')['ElCollapse']
